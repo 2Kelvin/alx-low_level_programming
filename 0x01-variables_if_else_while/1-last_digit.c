0d93c369946202c2ea0d26d5b38490cf8f813419
2Kelvin <rocketmancodes02@gmail.com>
2023-03-17 01:23:26 -0700

    Check the last digit of a number using modulo operator

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - returns the last digit of number n
+ *Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	int lastDigit = n % 10;
+	if (lastDigit > 5) 
+		printf("Last digit of n is %d and is greater than 5\n", lastDigit);
+	else if (lastdigit == 0) 
+		printf("Last digit of n is %d and is 0\n", lastDigit);
+	else if (lastDigit < 6 && lastDigit != 0)
+		printf("Last digit of n is %d and is less than 6 and not 0\n", lastDigit);
+	return (0);
+}
