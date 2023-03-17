fd129f4aa44cd75d3682ff63073faea51c62ba54
2Kelvin <rocketmancodes02@gmail.com>
2023-03-17 08:25:07 -0700

    Fix task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,9 @@ int main(void)
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-		if ((i == 'q') || (i == 'e'))
+		if ((i != 'q') &&  (i != 'e'))
 			putchar(i);
-		else
-			continue;
+
 	putchar('\n');
 	return (0);
 }
