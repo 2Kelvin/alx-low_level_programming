31b1a87dac64b39af975401d1ae70bbcc7da4fc1
2Kelvin <rocketmancodes02@gmail.com>
2023-03-21 09:02:54 -0700

    Fix task 7 and add all header functions in main.h

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-  * main - print last digit of anumber
+  * print_last_digit - print last digit of a number
+  * @x: is the number argument passed
   * Return: lastDigit
   */
 int print_last_digit(int x)
@@ -8,5 +9,5 @@ int print_last_digit(int x)
 	lastDigit = x % 10;
 	_putchar(lastDigit);
 	_putchar('\n');
-	return lastDigit;
+	return (lastDigit);
 }
