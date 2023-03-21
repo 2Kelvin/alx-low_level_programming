01c55f027e4aa0359671f289027aa47b2bd43bda
2Kelvin <rocketmancodes02@gmail.com>
2023-03-21 08:13:26 -0700

    Fix task 10 comments

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
   * add - add two numbers
-  * parameter 1: receives an int argument
-  *parameter 2: receives an int argument
+  * @parameter1: receives an int argument
+  *@parameter2: receives an int argument
   * Return: result
   */
 int add(int a, int b)
 {
 	int result = a + b;
+
 	return (result);
 }
