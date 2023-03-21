69ad35ac35ffa3a56559eaa69fa4da195a155ae3
2Kelvin <rocketmancodes02@gmail.com>
2023-03-21 07:50:31 -0700

    Correct task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,13 @@
   * print_alphabet - print lowercase letters
   * Return: void
   */
+void print_alphabet(void);
+
 void print_alphabet(void)
 {
 	int l;
 
 	for (l = 'a'; l <= 'z'; l++)
 		_putchar(l);
-	putchar('\n');
+	_putchar('\n');
 }
