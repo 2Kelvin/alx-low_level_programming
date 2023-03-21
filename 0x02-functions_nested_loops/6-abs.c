93c0b87f5b6c6aff0a328a32d19449730a648665
2Kelvin <rocketmancodes02@gmail.com>
2023-03-21 08:20:05 -0700

    Fix task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
   * main - priint the absolute value
+  * @x: int argument
   * Return: x
   */
 int _abs(int x)
@@ -8,6 +9,6 @@ int _abs(int x)
 	if (x < 0)
 		x = x * (-1);
 	else
-		x = x;
+		x;
 	return (x);
 }
