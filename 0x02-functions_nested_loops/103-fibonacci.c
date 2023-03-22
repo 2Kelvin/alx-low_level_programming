c0fa873d924dc37243696abb85257519f06fa404
2Kelvin <rocketmancodes02@gmail.com>
2023-03-21 17:54:22 -0700

    Add even fibonacci numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 /**
- * main - print sum of even numbers in the Fibonacci sequence
- * Return: 0
- */
+  * main - print sum of Fibonacci even numbers
+  * Return: 0
+  */
 int main(void)
 {
-        int numberCount = 3;
-	long int firstNo = 1;
-        long int secondNo = 2;
-        long int nextNo = firstNo + secondNo;
+	long int numCount = 3;
+	long int firstNum = 1;
+	long int secondNum = 2;
+	long int nextNum = firstNum + secondNum;
 	long int evenSum = 0;
 
-        printf("%lu, ", firstNo);
-        printf("%lu, ", secondNo);
-        while (numberCount <= 4000000)
-        {
-                if (firstNo % 2 == 0)
-			evenSum += firstNo;
-                firstNo = secondNo;
-                secondNo = nextNo;
-                nextNo = firstNo + secondNo;
-                numberCount++;
-        }
+	printf("%lu, ", firstNum);
+	printf("%lu, ", secondNum);
+	while (numCount <= 4000000)
+	{
+		if (firstNum % 2 == 0)
+			evenSum += firstNum;
+		firstNum = secondNum;
+		secondNum = nextNum;
+		nextNum = firstNum + secondNum;
+		numCount++;
+	}
 	printf("%lu\n", evenSum);
-        return (0);
+	return (0);
 }
