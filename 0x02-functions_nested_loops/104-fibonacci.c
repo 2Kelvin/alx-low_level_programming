d479cae8b61252de56201971af9db1917c1c247c
2Kelvin <rocketmancodes02@gmail.com>
2023-03-21 18:30:43 -0700

    Add task 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+  * main - first 98 Fibonacci numbers
+  * Return: 0
+  */
+int main(void)
+{
+	int count;
+	int f = 1;
+	int s = 2;
+	int n = f + s;
+
+	printf("%i, ", f);
+	printf("%i, ", s);
+	while (count <=98)
+	{
+		if (count == 98)
+			printf("%i\n", n);
+		else
+			printf("%i ", n);
+		f =s;
+		s = n;
+		n = f + s;
+		count++;
+	}
+	return (0);
+}
+	
