4649db647d79f9c6f52d0cfece210ba9fe472d16
2Kelvin <rocketmancodes02@gmail.com>
2023-03-22 22:23:35 -0700

    Print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * print_numbers - print number 0 to 9
+  * Return: void
+  */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; 0 <= 9; n++)
+		_putchar(n);
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@ int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
+void print_numbers(void);
