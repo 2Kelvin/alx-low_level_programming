e68eae778014266f30824fdf330d9e9a31e85bfa
2Kelvin <rocketmancodes02@gmail.com>
2023-03-22 22:35:07 -0700

    Print numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * more_numbers - print 0-14 10 times
+  * Return: void
+  */
+
+void more_numbers(void)
+{
+	int t, n;
+
+	for (t = 0; t < 10; t++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+}
