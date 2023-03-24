5fcbb1247e25197dad40156ec5ffe03657f6a8c3
2Kelvin <rocketmancodes02@gmail.com>
2023-03-23 17:32:00 -0700

    Fix  task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
-  * print_numbers - print number 0 to 9
-  * Return: void
-  */
+ * print_numbers - print number 0 to 9
+ * Return: void
+ */
 
 void print_numbers(void)
 {
 	int n;
 
 	for (n = 0; n <= 9; n++)
+	{
 		_putchar(n);
+	}
 }
