778dea98827e7f6456def783863679b3706cfa39
2Kelvin <rocketmancodes02@gmail.com>
2023-03-23 17:48:35 -0700

    Fix task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,18 +2,14 @@
 
 /**
   * _isdigit - check if its number 0..9
+  * @c: int parameter
   * Return: 1 if true 0 is if not
   */
 
 int _isdigit(int c)
 {
-	int i;
-
-	for (i = 0; i <= 9; i++)
-	{
-		if (c == i)
-			return (1);
-		else
-			return (0);
-	}
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
 }
