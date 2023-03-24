b477cb9482321fc1135b22ee6974132e2f2223fe
2Kelvin <rocketmancodes02@gmail.com>
2023-03-23 18:39:33 -0700

    Print diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,12 +13,12 @@ void print_diagonal(int n)
 	else
 	{
 		char space = ' ';
-		char slash = '\';
+		char slash = '\\';
 		int i, s;
 
 		for (i = 0; i < n; i++)
 		{
-			for (s = 0; s < n; s++)
+			for (s = 0; s < i; s++)
 				_putchar(space);
 			_putchar(slash);
 			_putchar('\n');
