f0d58003733c5aebc735db2129334a306f97848e
2Kelvin <rocketmancodes02@gmail.com>
2023-03-23 17:57:32 -0700

    Fix task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,11 @@
 
 void print_most_numbers(void)
 {
-	int n;
+	char n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		if (n != 2 || n != 4)
+		if (n != '2' || n != '4')
 			_putchar(n);
 	}
 	_putchar('\n');
