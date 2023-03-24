2a885afe32915662c5d99c1da9d45872e5fefd15
2Kelvin <rocketmancodes02@gmail.com>
2023-03-23 18:13:42 -0700

    Fix task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,8 @@ void more_numbers(void)
 		for (n = 0; n <= 14; n++)
 		{
 			if (n >= 10)
-			{
-				_putchar('1');
-				_putchar(((n % 10) + '0'));
-			}
-			else
-				_putchar(n);
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 		}
 		_putchar('\n');
 	}
