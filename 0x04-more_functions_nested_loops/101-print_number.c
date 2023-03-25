9f1adc0cecfe3cc81ea0ce824f0b3258092d2526
2Kelvin <rocketmancodes02@gmail.com>
2023-03-24 19:58:17 -0700

    Fix task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: input integer parameter
+ * print_number - print an integer
+ * @n: parameter
+ * Return: void
 **/
 
-void print_number(int n)
+void print_number(int c)
 {
-	unsigned int i = n;
+	unsigned int l = c;
 
-	if (n < 0)
+	if (c < 0)
 	{
 		_putchar(45);
-		i = -i;
+		l = -l;
 	}
-	if (i / 10)
+	if (l / 10)
 	{
-		print_number(i / 10);
+		print_number(l / 10);
 	}
-	_putchar(i % 10 + '0');
+	_putchar(l % 10 + '0');
 }
