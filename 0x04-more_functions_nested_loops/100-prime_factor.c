6c43a0fa572d3877439ea257e5f403577075d278
2Kelvin <rocketmancodes02@gmail.com>
2023-03-24 19:46:29 -0700

    Print an integer using putchar

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - prints the largest prime factor
+  * Return: 0 for success
+  */
+
+int main(void)
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_number - prints an integer
+  * @int: parameter
+  * Return: void
+  */
+
+void print_number(int c)
+{
+	unsigned int l = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		l = -l;
+	}
+
+	if (l / 10)
+		print_number(l / 10);
+
+	_putchar((l % 10) + '0');
+}
