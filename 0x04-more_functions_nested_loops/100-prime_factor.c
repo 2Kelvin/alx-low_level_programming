c321d0661fc59a6e535000b7d5913725da9a81c9
2Kelvin <rocketmancodes02@gmail.com>
2023-03-24 22:14:23 -0700

    Edit task 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,3 +7,16 @@
   */
 
 int main(void)
+{
+	int primeNum;
+	long int x = 612852475143;
+
+	for (primeNum = 2; primeNum > 1; primeNum++)
+	{
+		while (x % primeNum == 0)
+		{
+			x = x / primeNum;
+		}
+	}
+	printf("%d", primeNum);
+}
