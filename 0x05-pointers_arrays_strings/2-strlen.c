25e26c32e1e60656d692f48aa231b8089c5665d2
2Kelvin <rocketmancodes02@gmail.com>
2023-03-27 09:02:02 -0700

    Return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+/**
+  * _strlen(char *s) - returns the length of a string
+  * @s: pointer
+  * Return: the length of the string
+  */
+
+int _strlen(char *s)
+{
+	size_t length = strlen(*s);
+	return (length);
+}
