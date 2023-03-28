8fbabc2804313f0725172c1fe7148b2a73d92122
2Kelvin <rocketmancodes02@gmail.com>
2023-03-27 18:24:43 -0700

    Print characters at even indices

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main..h"
+
+/**
+  * puts2 - print string characters at even indices
+  * @str: pointer string
+  * Return: void
+  */
+
+void puts2(char *str)
+{
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		if (index % 2 == 0)
+			_putchar(str[index]);
+		index++;
+	}
+	_putchar('\n');
+}
