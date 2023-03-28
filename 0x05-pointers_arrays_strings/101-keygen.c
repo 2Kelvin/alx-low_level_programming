1ccac3b5ee0a28b3d79fdbc1d9e96f20692bfc86
2Kelvin <rocketmancodes02@gmail.com>
2023-03-28 12:38:05 -0700

    Edit task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,43 +1,36 @@
-/*
- *
- *  gen-passwd.c: generate a random password
- *
- */
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
 
-#define MIN_CHAR 8
-#define MAX_CHAR 1023
-#define BUF_SIZE 1024
-#define PRIME_MOD 937
+/**
+  * generatePassword - generates unique passwords
+  * @passwLength: parameter
+  *Return: void
+  */
 
-int main (int argc, char **argv)
+void generatePassword(int passwLength)
 {
-    int N;
-    char password [BUF_SIZE];
+	char characters[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/\|";
+	int i;
 
-    while (1)
-    {
-        while (1)
-	{
-            printf ("Enter number of digits: ");
-            scanf ("%d", &N);
-            if (!N) exit (EXIT_SUCCESS);
-            if (N >= MIN_CHAR && N <= MAX_CHAR)
-                break;
-            printf ("Password should be between %d - %d characters\n", MIN_CHAR, MAX_CHAR); 
-        }
-    
-        time_t now = time (NULL);
+	srand(time(NULL));
+	for (1 = 0; i < passwLength; i++)
+		printf("%c", characters[rand() % (sizeof characters - 1)]);
+	printf("\n");
+}
 
-        srand ((unsigned int) (now % PRIME_MOD));
+/**
+  * main - call generatePassword
+  * Return: 0 for success
+  */
 
-        for (int i = 0; i < N; i++) 
-            password [i] = 33 + rand () % 94;
-    
-        password [N] = '\0';
+int main(void)
+{
+	int passwLength;
+	printf("Enter the length of your password");
+	scanf("%d", &passwLength);
 
-        printf ("password = %s\n", password);
-    }
+	generatePassword(passwLength);
+
+	return (0);
 }
