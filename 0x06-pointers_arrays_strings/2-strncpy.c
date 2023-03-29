6c0e04893c9bdb40ff06f4e55dd4c38606b55a46
2Kelvin <rocketmancodes02@gmail.com>
2023-03-29 06:18:15 -0700

    Add _strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+/**
+  * _strncpy - copy string src to dest
+  * @dest: destination string pointer parameter
+  * @src: source string pointer parameter
+  * Return: dest
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while ((src[i] != '\0') && (i < n))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
