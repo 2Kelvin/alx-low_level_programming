c0ff2cae8dfc17bddaf5b5dbe19f22cde2aebd23
2Kelvin <rocketmancodes02@gmail.com>
2023-03-30 15:27:24 -0700

    Fix task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,16 +2,13 @@
 /**
  * cap_string - capitalizes all words of a string
  * @str: the string to change the first letter of a word in uppercase
- *
  * Return: capitalizes letters
  */
 char *cap_string(char *str)
 {
-	int i;
-	int j;
-	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
-
-	i = 0;
+	int i = 0;
+	int j = 0;
+	char specialChars[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
 	while (str[i] != '\0')
 	{
@@ -19,10 +16,9 @@ char *cap_string(char *str)
 		{
 			str[i] = str[i] - 32;
 		}
-		j = 0;
-		while (c[j] != '\0')
+		while (specialChars[j] != '\0')
 		{
-			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+			if (specialChars[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
 			{
 				str[i + 1] = str[i + 1] - 32;
 			}
