3d1d614d6b09088953e0ccb97ee75a14a3d3f19f
2Kelvin <rocketmancodes02@gmail.com>
2023-03-30 15:24:25 -0700

    Fix task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,34 @@
 #include "main.h"
-
 /**
-  * cap_string - capitalize 1st letter of each word
-  * @str: string parameter
-  * Return: str
-  */
-
+ * cap_string - capitalizes all words of a string
+ * @str: the string to change the first letter of a word in uppercase
+ *
+ * Return: capitalizes letters
+ */
 char *cap_string(char *str)
 {
-	int i = 0;
-	int j = 0;
-	char specialChars[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i;
+	int j;
+	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+
+	i = 0;
 
 	while (str[i] != '\0')
 	{
-		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-			str[i] = str[i] - 32;
-		while (specialChars[j] != '\0')
+		if (i == 0 && str[i] >= 97 && str[i] <= 122)
 		{
-			if (specialChars[j] == str[i] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			str[i] = str[i] - 32;
+		}
+		j = 0;
+		while (c[j] != '\0')
+		{
+			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+			{
 				str[i + 1] = str[i + 1] - 32;
+			}
 			j++;
 		}
-		i++
+		i++;
 	}
-
 	return (str);
 }
