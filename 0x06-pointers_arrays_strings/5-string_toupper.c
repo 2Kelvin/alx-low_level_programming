caf290464541f8c1aa368561d0b72389e4c709a0
2Kelvin <rocketmancodes02@gmail.com>
2023-03-30 12:58:36 -0700

    Lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+/**
+  * string_toupper - changes all letters from lowercase to uppercase
+  * @s - string parameter
+  * Return: uppercase string
+  */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] -32;
+	}
+
+	return (s);
+}
