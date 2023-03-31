6952da7e8a0860a7595cdddadf197febd71b5d6a
2Kelvin <rocketmancodes02@gmail.com>
2023-03-31 01:19:58 -0700

    Print integers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,11 +12,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		l = -l;
 	}
 	if (l / 10)
 	{
 		print_number(l / 10);
 	}
-	_putchar(l % 10 + '0');
+	_putchar(l % 10 + 48);
+}
