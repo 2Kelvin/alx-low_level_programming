dcfff398ba9f5eb64391632fc1b4c4941b0dfa0d
2Kelvin <rocketmancodes02@gmail.com>
2023-04-10 05:47:32 -0700

    Add unusd attribute to argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,14 +7,14 @@
   * Return: 0 (Success)
   */
 
-int main(int argc, char *argv[])
+int main(int __attribute__ ((unused)) argc, char *argv[])
 {
 	/* initialize index to loop through argv[] arguments */
 	int index;
 
 	/* print the 1st argv argument */
 	/* it should automatically be the program's name */
-	printf("%s\n", argv[argc - 1]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
