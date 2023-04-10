6e8057a1eb2ac1bf9b0eaaa813ec514fadcae6a8
2Kelvin <rocketmancodes02@gmail.com>
2023-04-10 05:58:45 -0700

    Update main

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,9 +10,6 @@
 
 int main(int __attribute__ ((unused)) argc, char *argv[])
 {
-	/* initialize index to loop through argv[] arguments */
-	int index;
-
 	/* print the 1st argv argument */
 	/* it should automatically be the program's name */
 	printf("%s\n", argv[0]);
