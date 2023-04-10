0d88d263db82711bcd2c4d59c7e28aaab7f7dd0e
2Kelvin <rocketmancodes02@gmail.com>
2023-04-10 06:33:28 -0700

    Print all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - print all arguments
+  * @argc: argument count
+  * @argv: argument strings
+  * Return: 0 for success
+  */
+
+int main(int argc, char *argv[])
+{
+	/* initializing index variable i */
+	/* will be used to loop thro arguments array */
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (i >= 1)
+			printf("%s/n", *argv[i]);
+	}
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,4 +1,4 @@
 ALX projects on function main's argc & argv arguments
-
 	- [x] Task 0 prints the name of the program
 	- [x] Task 1 prints argc (number of argv argument count)
+	- [x] Task 2 prints all arguments
