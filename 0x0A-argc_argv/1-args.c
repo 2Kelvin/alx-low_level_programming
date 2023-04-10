9ecc126ca25ec8f9844343c5c815926d12f07cbd
2Kelvin <rocketmancodes02@gmail.com>
2023-04-10 06:07:18 -0700

    Print argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - print argc
+  * @argc: argument count
+  * @argv: arguments
+  * Return: 0 for success
+  */
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	/* printing the number of arguments passed to main */
+	/* which basically means print argc */
+	printf("%d\n", argc);
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 ALX projects on function main's argc & argv arguments
 
-Task 0 prints the name of the program
+	- [x] Task 0 prints the name of the program
+	- [x] Task 1 prints argc (number of argv argument count)
