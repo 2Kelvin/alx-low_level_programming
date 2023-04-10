211ebd9479c5258ec7cf8008db0ae93e093fe0ec
2Kelvin <rocketmancodes02@gmail.com>
2023-04-10 06:11:09 -0700

    Fix main

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-  * main - print argc
-  * @argc: argument count
+  * main - print argument count
+  * @argc: arguments count
   * @argv: arguments
   * Return: 0 for success
   */
@@ -10,8 +10,8 @@
 int main(int argc, char __attribute__ ((unused)) *argv[])
 {
 	/* printing the number of arguments passed to main */
-	/* which basically means print argc */
-	printf("%d\n", argc);
+	/* which basically means print argc - the program argument */
+	printf("%d\n", (argc - 1));
 
 	return (0);
 }
