567db1502ef42128f6f426898e0d071dd358ecec
2Kelvin <rocketmancodes02@gmail.com>
2023-04-11 07:58:01 -0700

    Add type to malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	/* allocating memory space for pointerStr using malloc() */
-	pointerStr = malloc(size * sizeof(char));
+	pointerStr = (char *) malloc(size * sizeof(char));
 
 	if (pointerStr == NULL)
 		return (0);
