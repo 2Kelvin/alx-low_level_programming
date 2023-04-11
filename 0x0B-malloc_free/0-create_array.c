90ab874a01aa53e8e57c48f155245298a6eeb887
2Kelvin <rocketmancodes02@gmail.com>
2023-04-11 07:40:49 -0700

    Create an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+/**
+  * create_array - creates an array of chars
+  * @size: number of times to print char c
+  * @c: char to print
+  * Return: pointer array or NULL
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	/* initializing variables */
+	char *pointerStr;
+	unsigned int i;
+
+	/* checking if size is 0 */
+	/* if it is, returning (NULL) */
+	if (size == 0)
+		return (NULL);
+
+	/* allocating memory space for pointerStr using malloc() */
+	pointerStr = malloc((size + 1) * sizeof(char));
+	/* filling pointerStr with char c */
+	while (i < size)
+	{
+		pointerStr[i] = c;
+		i++;
+	}
+	/* inserting null terminator at the end of the string */
+	pointerStr[i] = '\0';
+
+	/* returning the pointer string */
+	return (pointerStr);
+}
