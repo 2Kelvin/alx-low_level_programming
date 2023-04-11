0b518844a67e0e49dacbded96a82c58050078644
2Kelvin <rocketmancodes02@gmail.com>
2023-04-11 15:17:24 -0700

    Edit 101 starter code

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,16 +9,30 @@
 char **strtow(char *str)
 {
 	char *wordsArr;
-	int strLength = 0;
+	int strLength = 0, i = 0, l = 0;
 
 	/* checking if the string passed in is NULL or empty */
 	if (str == NULL || str == "")
 		return (NULL);
-	/* calculating string length */
+	/* calculating string length without spaces or '\0' */
 	while (str[strLength] != '\0')
 		strLength++;
 	/* allocating memory */
-	wordsArr = malloc(sizeof(char) * strLength);
+	wordsArr = malloc(sizeof(char) * (strLength + 1));
 	/* checking if malloc wasn't successful */
 	if (wordsArr == NULL)
 		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		while (str[l] != ' ')
+		{
+			wordsArr[l] = str[l];
+			l++;
+		}
+		wordsArr[i] = str[i];
+		i++;
+	}
+
+	return (*wordsArr[i][l]);
+}
