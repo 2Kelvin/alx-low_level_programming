03759e2dcaa8c51c76e3e9200c4879f35379544d
2Kelvin <rocketmancodes02@gmail.com>
2023-04-13 11:19:19 -0700

    Add _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+/**
+  * _memset - fill memory with 0
+  * @s: pointer
+  * @b: character to fill in (0)
+  * @n: how many times
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
+
+/**
+  * _calloc - allocates memory for an array of nmemb items each size bytes
+  * @nmemb: number of items in array
+  * @size: bytes of each nmemb
+  * Return: nArr
+  */
+
+void *_calloc (unsigned int nmemb, unsigned int size)
+{
+	char *nArr;
+
+	/*  checking if nmemb or size is 0 */
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	/* allocating memory for nArr */
+	nArr = malloc(nmemb * size);
+	/* checking if memory was allocated to nArr successfully */
+	if (nArr == NULL)
+		return (NULL);
+	/* filling each nArr item (nmemb) with 0 */
+	_memset(nArr, 0, (nmemb * size));
+	return (nArr);
+}
