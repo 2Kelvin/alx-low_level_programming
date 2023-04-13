c84b8467944c313b2bcb07e259df90a6ff0af532
2Kelvin <rocketmancodes02@gmail.com>
2023-04-13 15:07:24 -0700

    Fix task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,24 +10,34 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *realPtr;
-	int i;
+	char *realPtr, *malPtr;
+	unsigned int n;
 
-	if (new_size > old_size)
-	{
-		realPtr = malloc(new_size);
-		for (i = 0; i < old_size; i++)
-			ptr[i] = realPtr[i];
-	}
-	else if (new_size == old_size)
-		return (ptr);
-	else if (ptr == NULL)
-		realPtr = malloc(new_size);
-	else if (new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	else if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+		return (malloc(new_size));
+	/* new reallocated memory */
+	realPtr = malloc(new_size);
+	if (realPtr == NULL)
+		return (NULL);
+	malPtr = ptr;
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			realPtr[i] = malPtr[i];
+	}
+	else if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			realPtr[i] = malPtr[i];
+	}
 
+	free(ptr);
 	return (realPtr);
 }
