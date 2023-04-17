24a044970df48c50551f9a8e44433d775f31c4c4
2Kelvin <rocketmancodes02@gmail.com>
2023-04-17 13:58:43 -0700

    Fix printf

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,15 +11,11 @@ void print_dog(struct dog *d)
 {
 	/* print nothing if d is NULL */
 	if (d == NULL)
-		printf("");
+		return;
 	/* if any object properties is NULL print (nil) */
 	if (d->name == NULL || d->age == 0 || d->owner == NULL)
 		printf("(nil)");
 	/* if everything is ok, print all the props */
 	else
-	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
-	}
+		printf("Name: %s\n, Age: %f\n, Owner: %s\n", d->name, d->age, d->owner);
 }
