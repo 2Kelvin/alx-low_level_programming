f6214af29f280e6a7639f624471f0f576b288de5
2Kelvin <rocketmancodes02@gmail.com>
2023-04-17 14:10:47 -0700

    Fix task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,5 +19,5 @@ void print_dog(struct dog *d)
 		d->owner =  "(nil);
 	/* if everything is ok, print all the props */
 	else
-		printf("Name: %s\n, Age: %f\n, Owner: %s\n", d->name, d->age, d->owner);
+		printf("Name : % s\nAge : % f\nOwner : % s\n", d->name, d->age, d->owner);
 }
