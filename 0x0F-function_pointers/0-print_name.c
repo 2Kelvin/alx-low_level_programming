13cc31e75a9972522d0f0a633fd0893aabc20e43
2Kelvin <rocketmancodes02@gmail.com>
2023-04-19 13:41:17 -0700

    Fix task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdlib.h>
+
 /**
   * print_name - print name using function pointer
   * @name: string pointer
@@ -10,7 +10,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 	/* checking if a name & function pointer have been passed in */
-	if (f != NULL && name != NULL)
+	if (f != NULL || name != NULL)
 		/* use the function pointer passed in to print any passsed in name */
 		f(name);
 	else
