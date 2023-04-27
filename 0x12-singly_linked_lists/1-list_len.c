30dc1c82a98aeda89cbd2360e3839c03ff0e900f
2Kelvin <rocketmancodes02@gmail.com>
2023-04-27 16:32:40 -0700

    Trial fix 2

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,9 +5,9 @@
 /**
   * list_len - returns no of elements in linked list list_t
   * @h: pointer to linked list
+  *
   * Return: no of nodes
   */
-
 size_t list_len(const list_t *h)
 {
 	size_t noOfNodes = 0;
