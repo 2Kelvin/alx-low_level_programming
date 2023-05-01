ea8a58aaa8372bf5481c0ea1eca5f12c4cd0cd41
2Kelvin <rocketmancodes02@gmail.com>
2023-05-01 07:06:01 -0700

    Add new head node fun

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * add_nodeint - add node at the head of a linked list
+  * @head: address of previous head node
+  * @n: int data
+  * Return: a pointer to the new head node
+  */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	/* dynamically creating the new node */
+	listint_t *newHeadNode = malloc(sizeof(listint_t));
+
+	/* returning NULL if creation is unsuccessful */
+	if (newHeadNode == NULL)
+		return (NULL);
+
+	/* assigning data & pointer to the new node */
+	newHeadNode->n = n;
+	newHeadNode->next = *head;
+
+	/* reassigning head pointer to carry the address of the new head node */
+	*head = newHeadNode;
+
+	return (*head);
+	free(newHeadNode);
+}
