5886e3de3afa79acb74acdc6e34199fd9b385870
2Kelvin <rocketmancodes02@gmail.com>
2023-05-01 07:18:54 -0700

    Include stdlib

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
   * add_nodeint - add node at the head of a linked list
@@ -25,4 +25,5 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	*head = newHeadNode;
 
 	return (newHeadNode);
+	free(newHeadNode);
 }
