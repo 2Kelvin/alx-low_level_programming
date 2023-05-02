91f9d4bda082ebe85be9d059af8c1ba95b27b548
2Kelvin <rocketmancodes02@gmail.com>
2023-05-02 15:35:26 -0700

    Delete node at given index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+  * pop_listint - deletes head node & returns next node's data
+  * @head: double pointer to linked list
+  * Return: next head node's data
+  */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *nextHead;
+	int nextHeadData;
+
+	/* if there's no linked list return 0 */
+	if (*head == NULL)
+		return (0);
+	/* accessing the 2nd node in the list */
+	nextHead = (*head)->next;
+	/* assigning it's data, stored in n */
+	nextHeadData = (*head)->n;
+	/* deleting the old head from memory */
+	free(*head);
+	/* making the next node the head */
+	*head = nextHead;
+
+	return (nextHeadData);
+}
+
+/**
+  * delete_nodeint_at_index - deletes the node at the index passed in
+  * @head: double pointer to list
+  * @index: node position in list
+  * Return: 1 (successful) or -1 (failed)
+  */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *currentPtr = *head;
+	listint_t *temporaryPtr = NULL;
+
+	if (index == 0)
+		return (pop_listintt(head));
+	for (i = 0; i < (index - 1); i++)
+	{
+		if (currentPtr->next == NULL)
+			return (-1);
+		currentPtr = currentPtr->next;
+	}
+	if (currentPtr->next == NULL)
+		return (-1);
+	temporaryPtr = currentPtr->next;
+	currentPtr->next = temporaryPtr->next;
+	free(temporaryPtr);
+
+	return (1);
+}
