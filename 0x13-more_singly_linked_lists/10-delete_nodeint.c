9aed6704756d5fbeeffdd980be2c4a8523e24074
2Kelvin <rocketmancodes02@gmail.com>
2023-05-02 15:49:25 -0700

    Fix task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,32 +2,6 @@
 #include <stdlib.h>
 
 /**
-  * pop_listint - deletes head node & returns next node's data
-  * @head: double pointer to linked list
-  * Return: next head node's data
-  */
-
-int pop_listint(listint_t **head)
-{
-	listint_t *nextHead;
-	int nextHeadData;
-
-	/* if there's no linked list return 0 */
-	if (*head == NULL)
-		return (0);
-	/* accessing the 2nd node in the list */
-	nextHead = (*head)->next;
-	/* assigning it's data, stored in n */
-	nextHeadData = (*head)->n;
-	/* deleting the old head from memory */
-	free(*head);
-	/* making the next node the head */
-	*head = nextHead;
-
-	return (nextHeadData);
-}
-
-/**
   * delete_nodeint_at_index - deletes the node at the index passed in
   * @head: double pointer to list
   * @index: node position in list
@@ -36,23 +10,26 @@ int pop_listint(listint_t **head)
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i = 0;
-	listint_t *currentPtr = *head;
-	listint_t *temporaryPtr = NULL;
+	unsigned int i;
+	listint_t *currentPtr = NULL;
+	listint_t *temporaryPtr = *head;
 
+	if (*head == NULL)
+		return (-1);
 	if (index == 0)
-		return (pop_listintt(head));
+	{
+		*head = (*head)->next;
+		free(temporaryPtr);
+		return (1);
 	for (i = 0; i < (index - 1); i++)
 	{
-		if (currentPtr->next == NULL)
+		if (temporaryPtr == NULL || temporaryPtr->next == NULL)
 			return (-1);
-		currentPtr = currentPtr->next;
+		temporaryPtr = temporaryPtr->next;
 	}
-	if (currentPtr->next == NULL)
-		return (-1);
-	temporaryPtr = currentPtr->next;
-	currentPtr->next = temporaryPtr->next;
-	free(temporaryPtr);
+	currentPtr = temporaryPtr->next;
+	temporaryPtr->next = currentPtr->next;
+	free(currentPtr);
 
 	return (1);
 }
