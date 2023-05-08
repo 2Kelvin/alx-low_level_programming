8b2601054e42f2bbd095510084ab7c3a318704d1
2Kelvin <rocketmancodes02@gmail.com>
2023-05-08 11:57:13 -0700

    Fix typo

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * create_file: fun that creates a file
+  * create_file - fun that creates a file
   * @filename: file to create
   * @text_content: filename content
   * Return: 1 (success) and -1 (failure)
@@ -10,7 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int leng = 0;
-	int opFile, wrFile;
+	int op, wr;
 
 	/* checking if filename is NULL */
 	if (filename == NULL)
@@ -24,12 +24,12 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	/* opening & writing into new created file */
-	opFile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wrFile = write(opfile, text_content, leng);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(op, text_content, leng);
 
-	if (opFile == -1 || wrFile == -1)
+	if (op == -1 || wr == -1)
 		return (-1);
 
-	close(opFile);
+	close(op);
 	return (1);
 }
