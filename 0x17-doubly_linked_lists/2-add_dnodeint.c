d57d2d3144aa300652498e19c0d753cea34a0a87
2Kelvin <rocketmancodes02@gmail.com>
2023-06-17 05:33:01 -0700

    Add new head node

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * add_dnodeint - add a new node at the beginning of a doubly linked list
+  * @head: pointer to the doubly linked list's head pointer
+  * @n: node data
+  * Return: new head node
+  */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	if ((*head) == NULL)
+	{
+		dlistint_t *newHnode = malloc(sizeof(dlistint_t));
+
+		if (newHnode == NULL)
+			return (NULL);
+		newHnode->n = n;
+		newHnode->prev = NULL;
+		newHnode->next = NULL;
+		(*head) = newHnode;
+		return (newHnode);
+	}
+
+	dlistint_t *newHnode = malloc(sizeof(dlistint_t));
+
+	newHnode->n = n;
+	newHnode->prev = NULL;
+	newHnode->next = (*head);
+	(*head) = newHnode;
+	return (newHnode);
+}
