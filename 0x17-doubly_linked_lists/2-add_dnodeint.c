cfd726053840394f8f03de73dd4349f0f299a598
2Kelvin <rocketmancodes02@gmail.com>
2023-06-17 13:34:06 -0700

    Err 8 fix trial 0

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,23 +12,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newHnode;
 
-	if ((*head) == NULL)
-	{
-		newHnode = malloc(sizeof(dlistint_t));
-		if (newHnode == NULL)
-			return (NULL);
-		newHnode->n = n;
-		newHnode->prev = NULL;
-		newHnode->next = NULL;
-		(*head) = newHnode;
-		return (newHnode);
-	}
-
 	newHnode = malloc(sizeof(dlistint_t));
 	newHnode->n = n;
 	newHnode->prev = NULL;
 	newHnode->next = (*head);
-	(*head)->prev = newHnode;
+	if ((*head) != NULL)
+		(*head)->prev = newHnode;
 	(*head) = newHnode;
 	return (newHnode);
 }
