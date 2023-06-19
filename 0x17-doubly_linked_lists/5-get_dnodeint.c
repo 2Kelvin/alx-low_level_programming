49f236eb9abda10c126f32c76e5345b8c898a4f0
2Kelvin <rocketmancodes02@gmail.com>
2023-06-19 10:21:33 -0700

    Break from while loop

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,7 +18,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (h != NULL)
 	{
 		if (index == nodeIdx)
+		{
 			nodeFound = h;
+			break;
+		}
 		nodeIdx++;
 		h = h->next;
 	}
