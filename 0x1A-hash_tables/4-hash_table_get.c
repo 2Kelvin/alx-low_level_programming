b4bdaf4c875aece5f6d8a5f0facf762b0c981040
2Kelvin <rocketmancodes02@gmail.com>
2023-07-07 10:18:24 -0700

    Find key at index

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_get - rertirives a value associated with a key
+ * @ht: hash table
+ * @key: array item key
+ * Return: value at key or NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index, i;
+
+	if (ht == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = 0; i < ht->size; i++)
+	{
+		if (index == i)
+		{
+			if (ht->array[i] != NULL)
+				return (ht->array[i]->value);
+			else
+				return (NULL);
+		}
+	}
+
+	return (NULL);
+}
