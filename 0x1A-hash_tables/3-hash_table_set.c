aff9e12c98b90e3284e999414eb3378749c44bb4
2Kelvin <rocketmancodes02@gmail.com>
2023-07-07 08:40:42 -0700

    Fix 1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int i = 0;
+	unsigned long int i;
 	hash_node_t *nodePair;
 
 	if (strlen(key) == 0 || !value || !key || ht == NULL)
