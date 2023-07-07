e39a6833ac4b875ea0ce7102ccf064fce820fcbc
2Kelvin <rocketmancodes02@gmail.com>
2023-07-07 06:10:36 -0700

    Reformat code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,15 +9,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newHashTable = malloc(sizeof(size));
-	unsigned long int i;
 
 	if (newHashTable == NULL)
 		return (NULL);
+	newHashTable->size = size;
 	newHashTable->array = malloc(sizeof(hash_node_t *) * size);
 	if (newHashTable->array == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		newHashTable->array[i] = NULL;
 	newHashTable->size = size;
 
 	return (newHashTable);
