19a2d706ed305812201b3d0165d7b02f5fa539c9
2Kelvin <rocketmancodes02@gmail.com>
2023-07-07 10:26:14 -0700

    Fix err

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -17,6 +17,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 	for (i = 0; i < ht->size; i++)
 	{
 		if (index == i)
