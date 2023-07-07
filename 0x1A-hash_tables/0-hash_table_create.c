275247485f46fbadd5973d7a7ecd0adfaf78da7e
2Kelvin <rocketmancodes02@gmail.com>
2023-07-07 05:57:05 -0700

    Fix missing comma

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	newHashTable->size = size;
 	newHashTable->array = malloc(sizeof(hash_node_t) * size);
 	if (newHashTable->array == NULL)
-		return (NULL)
+		return (NULL);
 
 	return (newHashTable);
 }
