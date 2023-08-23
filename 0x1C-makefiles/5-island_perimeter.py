a3f4d92fa6d2adf418de9db32936f28d01ce839c
2Kelvin <rocketmancodes02@gmail.com>
2023-08-23 11:21:40 -0700

    #island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,4 +3,16 @@
 
 
 def island_perimeter(grid):
-	"""Returns the perimeter of grid island"""
+    """Returns the perimeter of grid island"""
+    e = 0
+    isl = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                isl += 1
+                if x > 0 and grid[x - 1][y] == 1:
+                    e += 1
+                if y > 0 and grid[x][y - 1] == 1:
+                    e += 1
+
+    return (isl * 4) - (e * 2)
