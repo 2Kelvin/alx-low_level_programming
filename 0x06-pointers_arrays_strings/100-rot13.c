654491ef9bc745e74ddc593c3b50170d8049c704
2Kelvin <rocketmancodes02@gmail.com>
2023-08-31 09:10:40 -0700

    Undo task 100 changes

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,20 +8,5 @@
 
 char *rot13(char *s)
 {
-	int g, h;
-	char *r = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *w = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-
-	for (g = 0; s[g] != '\0'; g++)
-	{
-		for (h = 0; r[h] != '\0`; h++)
-		{
-			if (r[h] == s[g])
-			{
-				s[g] = w[h];
-				break;
-			}
-		}
-	}
 	return (s);
 }
