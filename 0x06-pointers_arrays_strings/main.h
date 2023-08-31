5f3cad043d532fd391e65a6d31d880e1e02b0db0
2Kelvin <rocketmancodes02@gmail.com>
2023-08-31 08:31:24 -0700

    Fix typo & gursd file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -9,5 +12,7 @@ char *cap_string(char *str);
 char *leet(char *s);
 char *rot13(char *s);
 void print_number(int n);
-char infinite_add(char *n1, char *n2.  char *r, int size_r);
+char infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+#endif
